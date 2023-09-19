90e7096dd5fa3354e4176329237fe53c104dddc3
marin <marine.runavot@hotmail>
2023-09-19 15:10:37 +0200

    Ajoutez des fichiers projet.

---
+++ ConversionEntierBinaire/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace ConversionEntierBinaire
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBase10 = new System.Windows.Forms.TextBox();
+            this.txtBase2 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtBase10
+            // 
+            this.txtBase10.Location = new System.Drawing.Point(6, 21);
+            this.txtBase10.Name = "txtBase10";
+            this.txtBase10.Size = new System.Drawing.Size(266, 22);
+            this.txtBase10.TabIndex = 0;
+            this.txtBase10.TextChanged += new System.EventHandler(this.txtBase10_TextChanged);
+            // 
+            // txtBase2
+            // 
+            this.txtBase2.Location = new System.Drawing.Point(6, 21);
+            this.txtBase2.Name = "txtBase2";
+            this.txtBase2.Size = new System.Drawing.Size(266, 22);
+            this.txtBase2.TabIndex = 1;
+            this.txtBase2.TextChanged += new System.EventHandler(this.txtBase2_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtBase10);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(278, 52);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Base 10";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtBase2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 70);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(278, 53);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Base 2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 134);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Conversion";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBase10;
+        private System.Windows.Forms.TextBox txtBase2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ ConversionEntierBinaire/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConversionEntierBinaire
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private bool isBinaire(string text)
+        {
+            bool bin = true;
+            for (int i=0; i<text.Length; i++)
+            {
+                if (text[i]!='0' && text[i]!='1')
+                {
+                    bin = false;
+                    break;
+                }
+            }
+            return bin;
+        }
+        private string conversionBase10ToBase2(int base10)
+        {
+            string base2 = "";
+            while (base10!=0)
+            {
+                base2 = (base10 % 2) + base2;
+                base10 /= 2;
+            }
+            if (base2=="")
+            {
+                base2 = "0";
+            }
+            return base2;
+        }
+        private int conversionBase2ToBase10(string base2)
+        {
+            int base10 = 0;
+            if (isBinaire(base2))
+            {
+                for (int i=0; i<base2.Length; i++)
+                {
+                    base10 += int.Parse(base2.Substring(base2.Length - 1 - i, 1)) * (int)Math.Pow(2, i);
+                }
+            }
+            else
+            {
+                base10= -1;
+            }
+            return base10;
+        }
+
+        private void txtBase10_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                int base10 = int.Parse(txtBase10.Text);
+                txtBase2.Text = conversionBase10ToBase2(base10);
+            }
+            catch(Exception ex)
+            {
+                txtBase2.Text = "";
+                txtBase10.Text = "";
+            }
+        }
+        private void txtBase2_TextChanged(object sender, EventArgs e)
+        {
+            int b10 = conversionBase2ToBase10(txtBase2.Text);
+            if (b10== -1)
+            {
+                txtBase10.Text = "";
+                txtBase2.Text = "";
+            }
+            else
+            {
+                txtBase10.Text = b10.ToString();
+            }
+        }
+    }
+}
